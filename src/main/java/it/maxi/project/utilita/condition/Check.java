d1a81d4107d1668f7addb549d15867575d9e629b
msyaskov <ms.yaskov@gmail.com>
2022-12-19 18:35:33 +0300

    Updated Check class

--- src/main/java/it/maxi/project/utilita/condition/Check.java
+++ src/main/java/it/maxi/project/utilita/condition/Check.java
@@ -1,5 +1,7 @@
 package it.maxi.project.utilita.condition;
 
+import java.util.Comparator;
+
 public class Check {
 
     public static <T> T notNull(T t) {
@@ -12,6 +14,50 @@ public class Check {
         } else {
             return t;
         }
+    }
+
+    public static Object notNullOrElse(Object obj, Object defaultObj) {
+        return obj == null ? defaultObj : obj;
+    }
+
+    public static boolean isNull(Object obj) {
+        return obj == null;
+    }
+
+    public static <T> boolean equals(T x, T y) {
+        return java.util.Objects.equals(x, y);
+    }
+
+    public static <T> boolean isMoreOrEqualThan(T x, T y, Comparator<T> comparator) {
+        return comparator.compare(x, y) >= 0;
+    }
+
+    public static <T> boolean isMoreThan(T x, T y, Comparator<T> comparator) {
+        return comparator.compare(x, y) > 0;
+    }
+
+    public static <T> boolean isLessThan(T x, T y, Comparator<T> comparator) {
+        return comparator.compare(x, y) < 0;
+    }
+
+    public static <T> boolean isLessOrEqualThan(T x, T y, Comparator<T> comparator) {
+        return comparator.compare(x, y) <= 0;
+    }
+
+    public static <T extends Comparable<T>> boolean isMoreOrEqualThan(T x, T y) {
+        return x.compareTo(y) >= 0;
+    }
+
+    public static <T extends Comparable<T>> boolean isMoreThan(T x, T y) {
+        return x.compareTo(y) > 0;
+    }
+
+    public static <T extends Comparable<T>> boolean isLessThan(T x, T y) {
+        return x.compareTo(y) < 0;
+    }
+
+    public static <T extends Comparable<T>> boolean isLessOrEqualThan(T x, T y) {
+        return x.compareTo(y) <= 0;
     }
 
 }
